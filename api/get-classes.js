b779ec64c3c713a0735d60007a06cb4e5c0810d2
b1rigu <gbilly2013@gmail.com>
2024-04-23 13:34:48 +0900

    test

--- api/get-classes.js
+++ api/get-classes.js
@@ -1,18 +1,26 @@
 import { getClasses } from "./scraper.js";
 
-export default async (event, context) => {
-    // Parse the request body from the event object
-    const { body } = JSON.parse(event.body);
+// export default async (event, context) => {
+//     // Parse the request body from the event object
+//     const { body } = JSON.parse(event.body);
 
-    // Your logic here
-    const allClasses = await getClasses(body.username, body.password, body.onetimepass);
+//     // Your logic here
+//     const allClasses = await getClasses(body.username, body.password, body.onetimepass);
 
-    // Return the response
-    return {
-        statusCode: 200,
-        body: JSON.stringify(allClasses),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
-};
+//     // Return the response
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify(allClasses),
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//     };
+// };
+
+export const config = {
+    runtime: 'edge'
+  }
+  
+export default async function handler(req) {
+    return new Response("Hello World");
+}
